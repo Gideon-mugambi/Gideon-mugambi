2330f57916b473eb06619f036688e5889d88b3ce
Gideon-mugambi <gidionmbiti1@gmail.com>
2025-03-11 00:05:23 +0300

    Add files via upload

---
+++ giddy.c
@@ -0,0 +1,18 @@
+/*
+do while
+author: Gideon Mugambi
+reg no:D33-8563-2021
+*/
+#include<stdio.h>
+
+int main()
+{
+int i=1;// start
+do {
+
+    printf("the number is %d and the cube of the %d is %d \n",i,i,i*i*i);
+    i++;// step
+    }
+    while (i<=100); // step
+    return 0;
+}
---
+++ gissy.c
@@ -0,0 +1,16 @@
+/*
+for loop
+author: Gideon mugsmbi
+reg no. D33-8563-2021
+*/
+#include<stdio.h>
+
+int main()
+{
+int i;
+for (i=1;i<=100;i++){
+
+    printf("the number is %d and cube of %d is %d \n", i,i,i*i*i);
+    }
+    return 0; // successful execution 
+}
